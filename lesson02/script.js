114626fba36d710edb80139c51931ba163a780e7
Zander <akma_2003@mail.ru>
2021-01-29 10:48:20 +0300

    Присвоение значений переменным по ТЗ второго урока пункт 2:
    Используя методы и свойства:
    
       - Вывести в консоль тип данных значений переменных money, income, deposit;
       - Вывести в консоль длину строки addExpenses
       - Вывести в консоль “Период равен (period) месяцев” и “Цель заработать (mission) рублей/долларов/гривен/юани”
       - Привести строку addExpenses к нижнему регистру и разбить строку на массив, вывести массив в консоль
       - Объявить переменную budgetDay и присвоить дневной бюджет (доход за месяц / 30)
       - Вывести в консоль budgetDay

--- lesson02/script.js
+++ lesson02/script.js
@@ -1,13 +1,29 @@
 // Переменные
-const money = 200000;
+const money = 210000;
 const income = 'зарплата';
-const addExpenses = 'интернет, такси, коммуналка, ипотека';
+const addExpenses = 'Интернет, Такси, Коммуналка, Ипотека';
 const deposit = false;
 const mission = 2000000;
 const period = 12;
 
-// модальное окно (alert)
-alert('Превед');
+// вывод сообщений в консоль
 
-// вывод сообщения в консоль
-console.log('Превед еще раз');
+// Вывести в консоль тип данных значений переменных money, income, deposit;
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+// Вывести в консоль длину строки addExpenses
+console.log(addExpenses.length);
+
+// Вывести в консоль “Период равен (period) месяцев” и “Цель заработать (mission) рублей/долларов/гривен/юани”
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+
+// Привести строку addExpenses к нижнему регистру и разбить строку на массив, вывести массив в консоль
+const addExpensesArr = addExpenses.toLowerCase().split(', ');
+console.log(addExpensesArr);
+
+// Объявить переменную budgetDay и присвоить дневной бюджет (доход за месяц / 30)
+const budgetDay = money / 30;
+console.log(budgetDay);
