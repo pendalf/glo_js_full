7277413000b4d8647da413b2affad42a04e153ca
Zander <akma_2003@mail.ru>
2021-02-16 11:20:39 +0300

    Выполнение усложненного задания урока №14 по ТЗ:
    1) Используя class DomElement из основного задания №1, создать квадрат 100 на 100 пикселей. Ему необходимо задать фон(background) любого цвета и свойство position: absolute.
    
    2) Поместить его на страницу только после выполнения события DOMContentLoaded.
    
    Внутри тега body  должно быть только подключение скрипта.
    
    3) Написать обработчик события для keydown, который будет принимать callback-функцию. Данная функция будет отлавливать нажатие на стрелки клавиатуры. В зависимости от нажатой кнопки(Вверх - стрелка вверх, Влево - стрелка влево, Вправо - стрелка вправо, Вниз - стрелка вниз) наш квадрат будет перемещаться на 10 пикселей.

--- lesson14_hard/script.js
+++ lesson14_hard/script.js
@@ -7,7 +7,11 @@ const DomElement = function(selector, options) {
     this.width = options.width;
     this.bg = options.bg;
     this.fontSize = options.fontSize;
-    this.text = options.text || 'null';
+    this.position = options.position;
+    this.text = options.text;
+    this.top = options.top;
+    this.left = options.left;
+    this.movingStep = options.movingStep || 10;
 
 };
 
@@ -20,29 +24,63 @@ DomElement.prototype.create = function() {
     }
     el.style.cssText = `height: ${this.height};
     width: ${this.width};
+    top: ${this.top};
+    left: ${this.left};
+    position: ${this.position};
     background: ${this.bg};
     font-size: ${this.fontSize};`;
     el.textContent = this.text;
+    this.el = el;
 
     document.body.append(el);
 };
 
+DomElement.prototype.moving = function(e) {
+    const computedSyle = getComputedStyle(this.el);
+    let axisY = 0,
+        axisX = 0;
+    switch (e.code) {
+        case 'ArrowUp':
+            axisY = -1;
+            break;
+        case 'ArrowDown':
+            axisY = 1;
+            break;
+        case 'ArrowRight':
+            axisX = 1;
+            break;
+        case 'ArrowLeft':
+            axisX = -1;
+            break;
+
+        default:
+            break;
+    }
+    if (axisY !== 0) {
+        let top = +computedSyle.top.replace('px', '');
+        top += axisY * this.movingStep;
+        this.el.style.top = top > 0 ? top + 'px' : 0;
+    }
+    if (axisX !== 0) {
+        let left = +computedSyle.left.replace('px', '');
+        left += axisX * this.movingStep;
+        this.el.style.left = left > 0 ? left + 'px' : 0;
+    }
+};
+
+DomElement.prototype.letMove = function() {
+    console.log(this.el);
+    document.addEventListener('keydown', this.moving.bind(this));
+};
+
 const block1 = new DomElement('.block', {
-    height: '50px',
-    width: '50px',
+    height: '100px',
+    top: 0,
+    left: 0,
+    width: '100px',
     bg: '#f00',
-    fontSize: '12px',
-    text: '.block'
+    position: 'absolute'
 });
 
 block1.create();
-
-const block2 = new DomElement('#best', {
-    // height: '50px',
-    // width: '50px',
-    // bg: '#00f',
-    // fontSize: '12px',
-    // text: '#block'
-});
-
-block2.create();
+block1.letMove();
