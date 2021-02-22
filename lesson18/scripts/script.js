e75f6765c4cc16fd97c6c3978b3ed1796e2f36cc
Zander <akma_2003@mail.ru>
2021-02-22 10:41:23 +0300

    Вырполнение задания урока №18 по ТЗ:
    2) Переписать таймер с помощью setInterval. Будьте внимательны, чтобы setInterval не вызывал сам себя, не превращался в рекурсивный вызов!
    
    3) Проверить, чтобы все работало и не было ошибок в консоли
    
    4) У таймера есть проблема (нужно исправить):
    
      - Если дата уже прошла, а мы заходим на страницу, то получим
    
    5) Изменить скрипт так, чтобы в таком случае выводилось: 00:00:00
    
      - Необходимо подставлять 0 перед значениями, которые состоят из одной цифры (из 4:6:50 сделает 04:06:50)

--- lesson18/scripts/script.js
+++ lesson18/scripts/script.js
@@ -4,32 +4,37 @@ window.addEventListener('DOMContentLoaded', () => {
     function countTimer(deadline) {
         const timerHours = document.querySelector('#timer-hours'),
             timerMinutes = document.querySelector('#timer-minutes'),
-            timerSeconds = document.querySelector('#timer-seconds');
+            timerSeconds = document.querySelector('#timer-seconds'),
+            timerInterval = setInterval(updateClock, 1000);
 
         function getTimeRemaining() {
             const dateStop = new Date(deadline).getTime(),
                 dateNow = new Date().getTime(),
-                timeRemainig = (dateStop - dateNow) / 1000,
+                timeRemainig = dateStop - dateNow >= 0 ? (dateStop - dateNow) / 1000 : 0,
                 seconds = Math.floor(timeRemainig % 60),
                 minutes = Math.floor(timeRemainig / 60) % 60,
                 hours = Math.floor(timeRemainig / 60 / 60) % 24;
             return { seconds, minutes, hours, timeRemainig };
         }
 
+        function twoGigits(val) {
+            return (val < 10 ? '0' : '') + val;
+        }
+
         function updateClock() {
             const timer = getTimeRemaining();
 
-            timerHours.textContent = timer.hours;
-            timerMinutes.textContent = timer.minutes;
-            timerSeconds.textContent = timer.seconds;
+            timerHours.textContent = twoGigits(timer.hours);
+            timerMinutes.textContent = twoGigits(timer.minutes);
+            timerSeconds.textContent = twoGigits(timer.seconds);
 
-            if (timer.timeRemainig > 1) {
-                setTimeout(updateClock, 1000);
+            if (timer.timeRemainig === 0) {
+                clearInterval(timerInterval);
             }
         }
         updateClock();
     }
 
-    countTimer('22 feb 2021 10:25:30');
+    countTimer('22 feb 2021 10:50');
 
 });
