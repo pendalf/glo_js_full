5903f03ba79dab72b042d4980202632c921c62df
Zander <akma_2003@mail.ru>
2021-03-12 00:08:09 +0300

    Выполнение простого задания урока №31 по ТЗ:
    В этом меню у нас есть три листа с классами:
    
    1. dropdown-lists__list--default - В нем должна содержаться информация о всех странах и ТОП 3 город каждой из этих стран. Данный список должен открываться по клику на инпут.
    
    2. dropdown-lists__list--select - Список, в котором должна отображаться информация о всех городах выбранной страны.
    
        -  Открывается когда кликаем на линию со страной - dropdown-lists__total-line
    
        -  Возвращается обратно к dropdown-lists__list--default, когда кликаем на dropdown-lists__total-line уже внутри dropdown-lists__list--select
    
    3. dropdown-lists__list--autocomplete - Список, который будет открываться, когда мы будем вводить что-то в инпут.
    
         - Другие списки при этом скрываются.
    
        - В этом списке будет отображаться информация из нашего объекта со схожими значениями. Ввели букву "М" и в списке должны отобразиться города начинающиеся на эту букву.
    
        - Cкрипт должен работать так, чтобы регистр не имел значение.
    
        - Если сходство не найдено, то выводим надпись "Ничего не найдено".
    
        - Если стираем значение, тогда отображается список dropdown-lists__list--default.
    
    В нашей верстке для примера уже введены стартовые данные, чтобы вы могли посмотреть структуру, которую вам нужно будет генерировать для вывода списка. Все, что внутри dropdown-lists__col вам нужно будет удалить.
    
    Так же, когда мы кликаем на страну или город в любом из списков, то значение внутри этого поля должно вставляться в input(value), появляется крестик close-button.
    
        - Если жмем на этот крестик, тогда стирается значение из input и закрывается выпадающее меню.
    
    Если кликаем на город, то в кнопку button должна вставляться ссылка link, ведущая на вики этого города.
    
        - Если значение стираем или нажимаем на крестик, то кнопка блокируется (изначально должна быть заблокированной)

--- inputCities/src/modules/dropdownList.js
+++ inputCities/src/modules/dropdownList.js
@@ -1,54 +1,188 @@
-const dropdownList = () => {
+const dropdownList = countries => {
     const inputCities = document.querySelector('.input-cities'),
-        dropdownLists = document.querySelector('.dropdown-lists__list'),
+        label = document.querySelector('.label'),
+        dropdownLists = document.querySelectorAll('.dropdown-lists__list'),
         dropdownDefault = document.querySelector('.dropdown-lists__list--default'),
         dropdownSelect = document.querySelector('.dropdown-lists__list--select'),
         dropdownAutocomplete = document.querySelector('.dropdown-lists__list--autocomplete'),
         input = document.getElementById('select-cities'),
         button = document.querySelector('.button'),
-        closeButton = document.querySelector('.close-button');
+        closeButton = document.querySelector('.close-button'),
+        localeList = new Map(),
+        localeDefaultList = new Map(),
+        localeCityList = new Map(),
+        lang = 'RU';
+
 
     // dropdownDefault.style.display = 'none';
 
-    const hide = el => {
-        el.style.display = 'none';
+    const hide = els => {
+        if (els instanceof HTMLElement) els = [els];
+        els.forEach(el => {
+            el.style.display = 'none';
+        });
     };
-    const show = el => {
-        el.style.display = 'block';
+    const show = els => {
+        if (els instanceof HTMLElement) els = [els];
+        els.forEach(el => {
+            el.style.display = 'block';
+        });
+
     };
-    const toggle = el => {
-        el.style.display = el.style.display === 'none' ? 'block' : 'none';
+    const toggle = els => {
+        if (els instanceof HTMLElement) els = [els];
+        els.forEach(el => {
+            el.style.display = el.style.display === 'none' ? 'block' : 'none';
+        });
+    };
+
+    const renderList = list => {
+        let output = `<div class="dropdown-lists__col">`;
+        list.forEach((v, k) => {
+            const { name, type, count } = v;
+            output += `<div class="dropdown-lists__${type === 'country' ? 'total-' : ''}line" data-key="${k}">
+                            <div class="dropdown-lists__${type}">${name}</div>
+                            <div class="dropdown-lists__count">${count}</div>
+                        </div>`;
+        });
+        output += `</div>`;
+        return output;
+    };
+
+    const generateKey = () => Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+
+    const getFlatList = () => {
+        countries[lang].forEach(v => {
+            localeList.set(generateKey(), {
+                type: 'country',
+                name: v.country,
+                count: v.count,
+            });
+            if (v.cities && v.cities.length) {
+                v.cities.forEach(v => {
+                    localeList.set(generateKey(), {
+                        type: 'city',
+                        name: v.name,
+                        count: v.count,
+                        link: v.link
+                    });
+                });
+            }
+        });
+    };
+
+    const getDefaultList = () => {
+        const list = new Map();
+        let i = 1;
+        localeList.forEach((v, k) => {
+            if (v.type === 'country') {
+                list.set(k, v);
+                i = 1;
+            } else if (i < 4) {
+                list.set(k, v);
+                i++;
+            }
+        });
+        return list;
+    };
+
+    const getCityes = key => {
+        const list = new Map();
+        let find = false;
+        localeList.forEach((v, k) => {
+            if (v.type === 'country' && k !== key) {
+                find = false;
+            }
+            if (find) {
+                list.set(k, v);
+            }
+            if (k === key && !find) {
+                find = true;
+                list.set(k, v);
+            }
+        });
+        return list;
+    };
+
+    const search = phrase => {
+        const list = new Map(),
+            regexp = new RegExp(phrase, 'gi');
+        localeList.forEach((v, k) => {
+            if (regexp.test(v.name)) {
+                list.set(k, v);
+            }
+        });
+        return list;
+    };
+
+    const reset = () => {
+        hide(dropdownLists);
+        button.classList.add('disabled');
+        button.href = '#';
+        input.value = '';
+        hide(closeButton);
+        show(label);
     };
 
     const handlers = () => {
         inputCities.addEventListener('click', e => {
             const target = e.target;
             if (target.id === 'select-cities') {
+                hide(dropdownLists);
                 show(dropdownDefault);
             }
-            if (target.closest('.dropdown-lists__total-line') && target.closest('.dropdown-lists__list--default')) {
-                hide(dropdownDefault);
+            if (target.closest('.dropdown-lists__total-line')) {
+                const key = target.closest('.dropdown-lists__total-line').dataset.key,
+                    cityes = getCityes(key);
+
+                hide(dropdownLists);
                 show(dropdownSelect);
+                dropdownSelect.innerHTML = renderList(cityes);
+                input.value = localeList.get(key).name;
+                show(closeButton);
+                hide(label);
+                // input.dispatchEvent(new Event('input'));
             }
             if (target === closeButton) {
-                input.value = '';
+                reset();
+            }
+            if (target.closest('.dropdown-lists__line')) {
+                const key = target.closest('.dropdown-lists__line').dataset.key,
+                    city = localeList.get(key);
+                input.value = localeList.get(key).name;
+                // input.dispatchEvent(new Event('input'));
+                button.classList.remove('disabled');
+                button.href = city.link;
+                show(closeButton);
+                hide(label);
+            }
+            if (target.classList.contains('disabled')) {
+                e.preventDefault();
             }
         });
 
         input.addEventListener('input', e => {
             const target = e.target;
             if (target.value === '') {
-                hide(closeButton);
+                reset();
             } else {
+                const list = search(target.value);
                 show(closeButton);
+                hide(label);
+                hide(dropdownLists);
+                show(dropdownAutocomplete);
+                dropdownAutocomplete.innerHTML = renderList(list);
             }
         });
     };
 
 
-    hide(dropdownDefault);
-    button.classList.add('disabled');
+    reset();
     handlers();
+    getFlatList();
+
+    console.log(localeList);
+    dropdownDefault.innerHTML = renderList(getDefaultList());
 };
 
 export default dropdownList;
